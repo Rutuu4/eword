2299adeb6ee3f61d47a3cd2adc38ed7a14e7d842
Rutuu4 <99796899+Rutuu4@users.noreply.github.com>
2025-10-09 13:58:11 +0530

    few changes

--- public_html/app/application/config/config.php
+++ public_html/app/application/config/config.php
@@ -1084,5 +1084,7 @@ $config['image_path']                   = $config['base_path']."images/";
 
 $config['admin_base_url']              	= $config['base_url']."admin/";
 $config['text_base_url']           		= "https://www.demanddeal.in/";
-putenv("TWILIO_ACCOUNT_SID=ACf78bbc77d2cf15a84e0d547207265974");
-putenv("TWILIO_AUTH_TOKEN=ae695fcbb40523824261ae3a659ae2b6");
+$config['twilio_sid'] = getenv('TWILIO_SID');
+$config['twilio_token'] = getenv('TWILIO_TOKEN');
+// putenv("TWILIO_ACCOUNT_SID=ACf78bbc77d2cf15a84e0d547207265974");
+// putenv("TWILIO_AUTH_TOKEN=ae695fcbb40523824261ae3a659ae2b6");
